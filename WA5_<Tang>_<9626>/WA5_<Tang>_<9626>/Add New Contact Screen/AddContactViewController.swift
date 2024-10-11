9a1ab92e1d605d125893793d6e76e361c438b5ca
imTang1100 <tangyifan.arch@gmail.com>
2024-10-11 15:32:47 -0400

    Initial commit2

--- WA5_<Tang>_<9626>/WA5_<Tang>_<9626>/Add New Contact Screen/AddContactViewController.swift
+++ WA5_<Tang>_<9626>/WA5_<Tang>_<9626>/Add New Contact Screen/AddContactViewController.swift
@@ -6,11 +6,15 @@
 //
 
 import UIKit
+import PhotosUI
 
 class AddContactViewController: UIViewController {
     
     var delegate: ViewController!
     var selectedType = "Cell"
+    
+    //MARK: variable to store the picked Image...
+    var pickedImage:UIImage?
     
     //MARK: initializing the ADDExpenseView...
     let addContactScreen = AddContactView()
@@ -29,9 +33,60 @@ class AddContactViewController: UIViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(
             title: "Save", style: .plain, target: self, action: #selector(onRightButtonTapped)
         )
-         
+        
+        addContactScreen.buttonTakePhoto.menu = getMenuImagePicker()
+        addContactScreen.buttonSelectType.menu = getMenuTypes()
 
         // Do any additional setup after loading the view.
+    }
+    
+    func getMenuImagePicker() -> UIMenu{
+        var menuItems = [
+            UIAction(title: "Camera",handler: {(_) in
+                self.pickUsingCamera()
+            }),
+            UIAction(title: "Gallery",handler: {(_) in
+                self.pickPhotoFromGallery()
+            })
+        ]
+        
+        return UIMenu(title: "Select source", children: menuItems)
+    }
+    
+    //MARK: take Photo using Camera...
+    func pickUsingCamera(){
+        let cameraController = UIImagePickerController()
+        cameraController.sourceType = .camera
+        cameraController.allowsEditing = true
+        cameraController.delegate = self
+        present(cameraController, animated: true)
+    }
+    
+    //MARK: pick Photo using Gallery...
+    func pickPhotoFromGallery(){
+        var configuration = PHPickerConfiguration()
+        configuration.filter = PHPickerFilter.any(of: [.images])
+        configuration.selectionLimit = 1
+        
+        let photoPicker = PHPickerViewController(configuration: configuration)
+        
+        photoPicker.delegate = self
+        present(photoPicker, animated: true, completion: nil)
+    }
+    
+    //MARK: menu for buttonSelectType setup...
+    func getMenuTypes() -> UIMenu{
+        var menuItems = [UIAction]()
+        
+        for type in Utilities.types{
+            let menuItem = UIAction(title: type,handler: {(_) in
+                                self.selectedType = type
+                                self.addContactScreen.buttonSelectType.setTitle(self.selectedType, for: .normal)
+                            })
+            menuItems.append(menuItem)
+        }
+        
+        return UIMenu(title: "Select type", children: menuItems)
     }
     
     @objc func onRightButtonTapped(){
@@ -144,5 +199,47 @@ extension AddContactViewController: UIPickerViewDelegate, UIPickerViewDataSource
         //MARK: updating the selected type when the user picks this row...
         selectedType = Utilities.types[row]
         return Utilities.types[row]
+    }
+}
+
+extension AddContactViewController:PHPickerViewControllerDelegate{
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        dismiss(animated: true)
+        
+        print(results)
+        
+        let itemprovider = results.map(\.itemProvider)
+        
+        for item in itemprovider{
+            if item.canLoadObject(ofClass: UIImage.self){
+                item.loadObject(ofClass: UIImage.self, completionHandler: { (image, error) in
+                    DispatchQueue.main.async{
+                        if let uwImage = image as? UIImage{
+                            self.addContactScreen.buttonTakePhoto.setImage(
+                                uwImage.withRenderingMode(.alwaysOriginal),
+                                for: .normal
+                            )
+                            self.pickedImage = uwImage
+                        }
+                    }
+                })
+            }
+        }
+    }
+}
+
+extension AddContactViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+        
+        if let image = info[.editedImage] as? UIImage{
+            self.addContactScreen.buttonTakePhoto.setImage(
+                image.withRenderingMode(.alwaysOriginal),
+                for: .normal
+            )
+            self.pickedImage = image
+        }else{
+            // Do your thing for No image loaded...
+        }
     }
 }
